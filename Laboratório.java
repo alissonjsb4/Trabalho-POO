7b22ba660d63b9804722deaf72973311ddac8ee7
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2023-10-25 16:41:48 -0300

    Create Laboratório.java
    Sala de Laboratório

---
+++ Laboratório.java
@@ -0,0 +1,9 @@
+public class Laboratório extends Sala{
+  private String EstadoComputador;
+  private int QuantidadeComputadores;
+  private int QuantidadeCadeiras;
+
+  public Laboratório(){
+    
+  }
+}
