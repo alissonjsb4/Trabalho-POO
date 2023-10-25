2651aae54b1679175bd3c34a6f2af12ed1341cda
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2023-10-25 12:35:30 -0300

    Create Sala.java
    I've started the first class we'll use in our project; this may be almost every class' parent!

---
+++ Sala.java
@@ -0,0 +1,10 @@
+public class Sala{
+  private String ArCondicionado;
+
+  public String setArcondicionado(String estado){
+    this.ArCondicionado = estado;
+  }
+  public String getArCondicionado{
+    return ArCondicionado;
+  }
+}
