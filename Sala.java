6b374a5ccb027e27a65bb8a3bba9b602cea7e026
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2023-10-27 20:56:46 -0300

    Update Sala.java

--- Sala.java
+++ Sala.java
@@ -4,9 +4,13 @@ public class Sala{
 
   public void setQuantidadeDeCadeiras(int Cadeiras){
     this.QuantidadeDeCadeiras = Cadeiras;
-    }  
+  }
   public void setArcondicionado(String estado){
     this.ArCondicionado = estado;
+  }
+
+  public int getQuantidadeDeCadeiras(){
+    return QuantidadeDeCadeiras;
   }
   public String getArCondicionado{
     return ArCondicionado;
