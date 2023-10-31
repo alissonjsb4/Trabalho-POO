dc36674e34339b2ea8be02198d638525917f7c10
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2023-10-31 14:43:53 -0300

    Create Sessao.java

---
+++ Sessao.java
@@ -0,0 +1,10 @@
+public class Sessao{
+  private Filme filme;
+  private String horario;
+  private Sala sala;
+  private int vagas;
+  private Usuario[] publico;
+
+  public boolean isSessaoDisponivel(){}
+  public void adicionarPessoa(){}
+}
