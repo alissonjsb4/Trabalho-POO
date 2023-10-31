337af24d47f867a4d6701df99f01400d9d6aeaa1
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2023-10-31 14:23:53 -0300

    Update and rename Auditorio.java to Sala.java

--- Auditorio.java
+++
@@ -1,3 +0,0 @@
-public class Auditorio extends Sala{
-  private 
-}
---
+++ Sala.java
@@ -0,0 +1,11 @@
+public class Sala{
+  private int salaID;
+  private int capacidade;
+  private boolean[][] mapaAssentos;
+
+  public void setTodosAssentosLivres(){
+  }
+  public boolean getStatusASsento(){}
+  public boolean isSalaLotada(){}
+  public void reservarAssento(int[][] cadeira){}
+}
