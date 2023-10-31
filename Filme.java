4a830682a3be98a4c450ab3db6be88a148c2a352
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2023-10-31 14:39:45 -0300

    Update Filme.java

--- Filme.java
+++ Filme.java
@@ -3,10 +3,18 @@ public class Filme{
 	private String titulo;
 	private String diretor;
 	private int classificacao;
-	private boolean is;
+	private boolean tresD;
 	private String sinopse;
 	private char faixaAudio;
 	private int duracao;
 	
-	
+	public String getCodigo(){}
+	public String getInfo(){}
+	public String getTitulo(){}
+	public String getDiretor(){}
+	public int getClassficacao(){}
+	public boolean isTresD(){}
+	public String getSinopse(){}
+	public char getFaixaAudio(){}
+	public int getDuracao(){}
 }
