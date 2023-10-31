e928028effe740308c62c680254c8a413f3c4a11
Vinicius <148991814+KViniSantos@users.noreply.github.com>
2023-10-31 15:13:55 -0300

    Create Login.java

---
+++ Login.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame {
+    private TextField campoUser;
+    private JPasswordField campoSenha;
+
+    public Login(){
+        super("CineFlop");
+
+        setLayout(new BorderLayout());
+        
+
+        campoUser = new TextField(10);
+        campoSenha = new JPasswordField(10);
+        JButton botaologin = new JButton("Login");
+
+        JPanel painel = new JPanel(new GridLayout(3, 2));
+        painel.add(new JLabel("Usuário: "));
+        painel.add(campoUser);
+        painel.add(new JLabel("Senha: "));
+        painel.add(campoSenha);
+        painel.add(new JLabel(""));
+        painel.add(botaologin);
+        
+        painel.setBackground(Color.ORANGE);
+        botaologin.setBackground(Color.BLACK);
+        botaologin.setForeground(Color.ORANGE);
+        
+
+        add(painel, BorderLayout.CENTER);
+
+        botaologin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                String user = campoUser.getText();
+                char[] senha = campoSenha.getPassword();
+
+                if (user.equals("usuario") && String.valueOf(senha).equals("senha")) {
+                    JOptionPane.showMessageDialog(null, "Login bem-sucedido!");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Login falhou. Tente novamente.");
+                }
+
+                campoUser.setText("");
+                campoSenha.setText("");
+            }
+        });
+
+        setSize(300, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Login();
+            }
+        });
+    }
+}
