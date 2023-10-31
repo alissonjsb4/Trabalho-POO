e5dd3d074da328a072b6785eb647d700af0e88c2
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2023-10-31 14:45:32 -0300

    Create IAdmin.java

---
+++ IAdmin.java
@@ -0,0 +1,8 @@
+public interface IAdmin{
+  public void adicionarFilme();
+  public void removerFilme();
+  public void adicionarSessao();
+  public void removerSessao();
+  public void adicionarSala();
+  public void removerSala();
+}
