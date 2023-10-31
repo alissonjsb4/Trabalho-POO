73332cc2f50695cf2dd854a4e8e46a6f7a8f61f7
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2023-10-31 14:35:45 -0300

    Create Filme.java

---
+++ Filme.java
@@ -0,0 +1,12 @@
+public class Filme{
+	private String codigo;
+	private String titulo;
+	private String diretor;
+	private int classificacao;
+	private boolean is;
+	private String sinopse;
+	private char faixaAudio;
+	private int duracao;
+	
+	
+}
