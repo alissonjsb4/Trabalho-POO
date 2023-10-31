5aa646732b1028fa04c6eb62d1a4e4d3c5ffde0d
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2023-10-31 14:42:05 -0300

    Create ICinema.java

---
+++ ICinema.java
@@ -0,0 +1,8 @@
+public interface ICinema{
+  public Filme procurarFilme(){}
+  public Sessao procurarSessao(){}
+  public Sala procurarSala(){}
+  public boolean existeFilme(){}
+  public boolean existeSessao(){}
+  public boolean existeSala(){}
+}
