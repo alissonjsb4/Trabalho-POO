534a466a0a96dadfe1dc42802861b162223b0ae9
Thiago Siqueira <91488917+thiago-sqr@users.noreply.github.com>
2023-11-13 13:55:06 -0300

    Update Filme.java

--- Filme.java
+++ Filme.java
@@ -7,14 +7,50 @@ public class Filme{
 	private String sinopse;
 	private char faixaAudio;
 	private int duracao;
-	
-	public String getCodigo(){}
-	public String getInfo(){}
-	public String getTitulo(){}
-	public String getDiretor(){}
-	public int getClassficacao(){}
-	public boolean isTresD(){}
-	public String getSinopse(){}
-	public char getFaixaAudio(){}
-	public int getDuracao(){}
+
+	public Filme(String codigo, 
+		     String titulo, 
+		     String diretor, 
+		     int classificacao, 
+		     boolean tresD, 
+		     String sinopse, 
+		     char faixaAudio, 
+		     int duracao) {
+		this.codigo = codigo;
+		this.titulo = titulo;
+		this.diretor = diretor;
+		this.classificacao = classificacao;
+		this.tresD = tresD;
+		this.sinopse = sinopse;
+		this.faixaAudio = faixaAudio;
+		this.duracao = duracao;	
+	}
+	public String getCodigo(){
+		return codigo;
+	}
+	public String getInfo(){
+		return "Título: "+getTitulo();
+		//terminar esse método
+	}
+	public String getTitulo(){
+		return titulo;
+	}
+	public String getDiretor(){
+		return diretor;
+	}
+	public int getClassficacao(){
+		return classificacao;
+	}
+	public boolean isTresD(){
+		return tresD;
+	}
+	public String getSinopse(){
+		return sinopse;
+	}
+	public char getFaixaAudio(){
+		return faixaAudio;
+	}
+	public int getDuracao(){
+		return duracao;
+	}
 }
