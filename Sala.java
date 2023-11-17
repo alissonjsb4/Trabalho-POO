38217073be9531ad6a3ffd649e6ba20a6c962ab0
Thiago Siqueira <91488917+thiago-sqr@users.noreply.github.com>
2023-11-17 19:21:16 -0300

    Construtor de Sala adicionado

--- Sala.java
+++ Sala.java
@@ -3,6 +3,12 @@ public class Sala{
   private int capacidade;
   private boolean[][] mapaAssentos;
 
+  public Sala (int salaID, int filas, int colunas) {
+    this.salaID = salaID;
+    this.capacidade = filas * colunas;
+    this.mapaAssentos = new boolean[filas][colunas];
+  }
+
   public void setTodosAssentosLivres(){
   }
   public boolean getStatusASsento(){}
