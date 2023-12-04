b808389c3c139cad9364d742902814ff2715134d
Josefflessa <148976170+Josefflessa@users.noreply.github.com>
2023-12-04 15:24:44 -0300

    Delete ICinema.java

--- ICinema.java
+++
@@ -1,8 +0,0 @@
-public interface ICinema {
-  public Filme procurarFilme(String codigo);
-  public Sessao procurarSessao(String codigo);
-  public Sala procurarSala(int SalaID);
-  public boolean existeFilme(String codigo);
-  public boolean existeSessao(String codigo);
-  public boolean existeSala(int SalaID);
-}
